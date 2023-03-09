5822b44a4a8b372ffce0e1443432dd7771f20809
ManSIayeR <103418605+Darek3399@users.noreply.github.com>
2023-03-09 17:33:17 +0500

    written html markup

---
+++ js/country_selection.js
@@ -0,0 +1,37 @@
+
+
+const requests = [new URL('../server/server-ok.json', import.meta.url), new URL('../server/server-error.json', import.meta.url)]
+
+
+const http = new XMLHttpRequest()
+
+http.onreadystatechange = () => {
+	if(http.readyState === 4 && http.status === 200) {
+		func(http.responseText)
+	}
+}
+
+http.open("GET", requests[1])
+http.send()
+
+const func = (data) => {
+	console.log(JSON.parse(data).response.statusText)
+}
+
+
+
+
+
+
+
+
+
+
+
+// const dateCheck = (day, month, year) => {
+// 	const date = new Date(year, --month, day)
+// 	console.log(date)
+// 	return day === date.getDate() && month === date.getMonth() && year === date.getFullYear()
+// }
+
+// console.log(dateCheck(9, 3, 2023))
