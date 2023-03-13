0631a773c49bf4fcbfd15fc10c8137e18a241bf5
ManSIayeR <103418605+Darek3399@users.noreply.github.com>
2023-03-13 18:31:04 +0500

    get years and countries "server"

--- js/country_selection.js
+++ js/country_selection.js
@@ -1,28 +1,38 @@
 const requests = [new URL('../server/server-ok.json', import.meta.url), new URL('../server/server-error.json', import.meta.url)]
+const countries = new URL('../server/countries_dates.json', import.meta.url)
+
+// get and set in html countries and years
+fetch(countries, {
+	method: `GET`, 
+	credentials: 'same-origin',
+}
+)
+.then( data => data.text())
+.then( data => {
 
 
-const http = new XMLHttpRequest()
+	//countries
+	const nationalitySelect = document.querySelector(`#nationality`)
+	const countriesArray = JSON.parse(data).response.countries
 
-http.onreadystatechange = () => {
-	if(http.readyState === 4 && http.status === 200) {
-		func(http.responseText)
+	for(let i = 0; i < countriesArray.length; i++) {
+
+		const newItem = document.createElement(`option`)
+		newItem.innerHTML = `<option value="${countriesArray[i]}">${countriesArray[i]}</option>`
+		nationalitySelect.appendChild(newItem)
 	}
-}
+	
+	//years
+	const yearsSelect = document.querySelector(`.birth-years`)
+	const yearsArray = JSON.parse(data).response.years
 
-http.open("GET", requests[1])
-http.send()
+	for(let i = 0; i < yearsArray.length; i++) {
 
-const func = (data) => {
-	// console.log(JSON.parse(data).response.statusText)
-}
-
-
-
-
-
-
-
-
+		const newItem = document.createElement(`option`)
+		newItem.innerHTML = `<option value="${yearsArray[i]}">${yearsArray[i]}</option>`
+		yearsSelect.appendChild(newItem)
+	}
+})
 
 
 
